e7754140576c6d1a8995b9b6853d9044fb2023e3
Michael Chen <michael.chen@applydigital.co>
2018-11-15 13:35:54 -0800

    test api

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../app');
 
+jest.setTimeout(20000);
+
 describe('Test api', () => {
     it('returns match history', async () => {
         const response = await request(app).get('/match/history/m1chaelchen');
